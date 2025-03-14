e1f125a3ad3ffb1919d40ab092bff3c7d3eb25bc
Nam <69331696+borealis2311@users.noreply.github.com>
2025-03-14 20:57:15 +0700

    First commit

--- be/main.py
+++ be/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, EmailStr
+
+class UserBase(BaseModel):
+    username: str
+    email: EmailStr
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Hello, FastAPI!"}
+
+@app.post("/users/")
+def create_user(user: UserBase):
+    return user    
